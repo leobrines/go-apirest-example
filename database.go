728c99c6d6ba22e6b56ab7ea8f90248d7ffe29a2
leobrines <leopoldo.brines@hotmail.com>
2019-10-26 12:47:08 -0300

    Add readability

--- database.go
+++ database.go
@@ -1,28 +1,29 @@
 package main
 
-import(
-	_ "github.com/go-sql-driver/mysql"
+import (
 	"database/sql"
-	"os"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
 type DatabaseManager struct {
 	db *sql.DB
 }
-func (this *DatabaseManager) start () {
+
+func (this *DatabaseManager) start() {
 	var err error
-	connection := os.Getenv("DBUSER")+":"+os.Getenv("DBPASSWD")+"@/"+os.Getenv("DBNAME")+"?charset=utf8"
+	connection := os.Getenv("DBUSER") + ":" + os.Getenv("DBPASSWD") + "@/" + os.Getenv("DBNAME") + "?charset=utf8"
 	this.db, err = sql.Open("mysql", connection)
 
 	if err != nil {
 		panic(err)
 	}
 
-	configureDB(this.db);
+	configureDB(this.db)
 }
 
-func (this *DatabaseManager) getItems () *[]Item {
+func (this *DatabaseManager) getItems() *[]Item {
 	rows, err := this.db.Query("SELECT * FROM item")
 
 	if (err) != nil {
@@ -50,12 +51,12 @@ func (this *DatabaseManager) getItems () *[]Item {
 	return &result
 }
 
-func configureDB (db *sql.DB) {
+func configureDB(db *sql.DB) {
 	createItemTable(db)
 	createItemExampleTable(db)
 }
 
-func createItemTable (db *sql.DB) {
+func createItemTable(db *sql.DB) {
 	_, err := db.Query("CREATE TABLE IF NOT EXISTS item (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), description VARCHAR(3000), price INT);")
 
 	if err != nil {
@@ -65,7 +66,7 @@ func createItemTable (db *sql.DB) {
 	fmt.Println("Tabla items creada!")
 }
 
-func createItemExampleTable (db *sql.DB) {
+func createItemExampleTable(db *sql.DB) {
 	_, err := db.Query("CREATE TABLE IF NOT EXISTS item_example (item_id INT, img VARCHAR(500), PRIMARY KEY (item_id), FOREIGN KEY (item_id) REFERENCES item (id));")
 
 	if err != nil {
